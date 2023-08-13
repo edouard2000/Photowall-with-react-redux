ffa78819ee9492fa4bcc00ce4613d071b03afdbb
Edouard Kwizera <94765310+KwizeraEdouard@users.noreply.github.com>
2023-08-13 02:51:15 -0400

    added remove button

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import Title from "./Title";
 
-const posts = [{
+const initialPosts = [{
     id: "0",
     description: "beautiful landscape",
     imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
@@ -15,22 +15,32 @@ const posts = [{
     description: "On a vacation!",
     imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
     }]
+  
+
+    
 
     function Main() {
+        const [posts, setPosts] = useState(initialPosts);
+    
+        const removePhoto = (postId) => {
+            const updatedPosts = posts.filter(post => post.id !== postId);
+            setPosts(updatedPosts);
+        };
+    
         return (
             <div className="photoGrid">
-              <Title title={"Photo wall"} />
-              {posts.map((post) => (
-                <div key={post.id} className="photoBox">
-                  <figure className="figure">
-                    <img src={post.imageLink} alt={post.description} className="photoImage"/>
-                    <figcaption className="photoCaption">{post.description}</figcaption>
-                  </figure>
-                  <button className="removeButton" onClick={() => console.log('Remove button clicked for post:', post.id)}>
-                    Remove
-                  </button>
-                </div>
-              ))}
+                <Title title={"Photo wall"} />
+                {posts.map((post) => (
+                    <div key={post.id} className="photoBox">
+                        <figure className="figure">
+                            <img src={post.imageLink} alt={post.description} className="photoImage"/>
+                            <figcaption className="photoCaption">{post.description}</figcaption>
+                        </figure>
+                        <button className="removeButton" onClick={() => removePhoto(post.id)}>
+                            Remove
+                        </button>
+                    </div>
+                ))}
             </div>
         );
     }
